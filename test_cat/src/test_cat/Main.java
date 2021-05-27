7c22d53a2bdcdbdef6858197539fe909994f72d6
DESKTOP-78MNPFP\lalal <lalalayu87@gmail.com>
2021-05-27 23:10:52 +0900

    고양이 테스트

---
+++ test_cat/src/test_cat/Main.java
@@ -0,0 +1,21 @@
+package test_cat;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("\\    /\\\n");
+		System.out.println(" )   ( \')\n");
+		System.out.println("(   /  )\n");
+		System.out.println(" \\ (__)|");
+
+	}
+
+}
+
+/*
+\    /\
+)   ( ')
+(  /  )
+\(__)|
+*/
