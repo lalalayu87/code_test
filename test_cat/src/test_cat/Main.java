ea2820b3ac963517e36d610cb2fdf700d1bd2355
DESKTOP-78MNPFP\lalal <lalalayu87@gmail.com>
2021-05-28 22:55:32 +0900

    고양이 커밋(..?)

--- test_cat/src/test_cat/Main.java
+++ test_cat/src/test_cat/Main.java
@@ -4,9 +4,9 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		System.out.println("\\    /\\\n");
-		System.out.println(" )   ( \')\n");
-		System.out.println("(   /  )\n");
+		System.out.println("\\   /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(   /  )");
 		System.out.println(" \\ (__)|");
 
 	}
