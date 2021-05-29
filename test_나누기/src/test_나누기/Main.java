5df0d2751da512ac4c983da49fdf77e96407822b
DESKTOP-78MNPFP\lalal <lalalayu87@gmail.com>
2021-05-29 12:13:26 +0900

    8) 사칙연산 A/B - 소수점까지 나오려면 double

---
+++ test_나누기/src/test_나누기/Main.java
@@ -0,0 +1,17 @@
+package test_나누기;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		Double A, B;
+		A = sc.nextDouble();
+		B = sc.nextDouble();
+		
+		System.out.println(A/B);
+	}
+
+}
